26c2de8117cf5aae09eea7f27ab371f9c53f1827
Laro DEE <larodi@gmail.com>
2016-12-09 23:16:09 +0200

    changes

--- oth/upgrade-dec.sh
+++ oth/upgrade-dec.sh
@@ -1,13 +1,20 @@
+if ! test -e ~/.ssh/id_ecdsa.pub; 
+then
+  echo === GENERATE ECDSA key ===
+  ssh-keygen -t ecdsa
+  ssh-copy-id -i ~/.ssh/id_ecdsa.pub pi-chan@dev2-bg.plan-vision.com -p2299
+fi
 
 if ! grep -q temp_limit /tmp/xxx; 
 then 
-cat >> /tmp/razgele <<EOF 
- gpu_mem=320
- arm_freq=1000
- sdram_freq=500
- core_freq=500
- over_voltage=2
- temp_limit=80 #Will throttle to default clock speed if hit.
+  echo === IMPROVE SPEED ===
+  cat >> /tmp/razgele <<EOF 
+    gpu_mem=320
+    arm_freq=1000
+    sdram_freq=500
+    core_freq=500
+    over_voltage=2
+    temp_limit=80 #Will throttle to default clock speed if hit.
 EOF
 fi
 

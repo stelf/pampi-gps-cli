40f7e375d84df030a207318c8e15c5e8af7e5d5a
Laro DEE <larodi@gmail.com>
2016-12-10 09:12:47 +0200

    separate root stuff

---
+++ oth/config-boot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+if ! grep -q temp_limit /boot/config.txt;
+then
+  echo === IMPROVE SPEED ===
+cat >> /boot/config.txt <<EOF
+ gpu_mem=320
+ arm_freq=1000
+ sdram_freq=500
+ core_freq=500
+ over_voltage=2
+ temp_limit=80 #Will throttle to default clock speed if hit.
+EOF
+fi
+
+raspi-config
+raspi-update
--- oth/upgrade-dec.sh
+++ oth/upgrade-dec.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo * security
 
 sudo dpkg-reconfigure console-setup
@@ -9,27 +11,12 @@ then
   ssh-copy-id -i ~/.ssh/id_ecdsa.pub pi-chan@dev2-bg.plan-vision.com -p2299
 fi
 
-echo * tuning
-
-if ! grep -q temp_limit /tmp/xxx;
-then
-  echo === IMPROVE SPEED ===
-cat >> /boot/config.txt <<EOF
- gpu_mem=320
- arm_freq=1000
- sdram_freq=500
- core_freq=500
- over_voltage=2
- temp_limit=80 #Will throttle to default clock speed if hit.
-EOF
-fi
-
 echo * apps
 
 apt update
 apt upgrade
 apt install xautomation chromium-browser
-apt install raspi-update lightdm 
+apt install raspi-update lightdm ack-grep
 apt install libgl1-mesa-dri xcompmgr lxde-core
 
 echo * autostart
@@ -42,5 +29,6 @@ echo # gps
 cp gpsd.socket /lib/systemd/system
 cp gpsd
 
-raspi-config
-raspi-update
+echo * tuning
+
+sudo ./config-boot.sh

be3a437d9bbed7aa8d38fae277550254d4cbd709
Laro DEE <larodi@gmail.com>
2016-12-14 13:44:23 +0200

    fix reloader

--- public/reloader.js
+++ public/reloader.js
@@ -1,25 +1,24 @@
 
 const ifr = document.getElementById('actual');
 
-// hostReachable() taken from
-// from: https://gist.github.com/jpsilvashy/5725579
-
 function whenRead() {
-  const loc = "http://dev2-bg.plan-vision.com:3000/?rand=" + Math.floor((1 + Math.random()) * 0x10000);
   let ifr = document.getElementById('appframe');
 
   setInterval(() =>
-    fetch(loc, {
-      mode: "no-cors"
+    fetch("http://dev2-bg.plan-vision.com:3000/?rand=" + Math.floor((1 + Math.random()) * 0x10000), {
+      mode: 'no-cors'
     }).then(res => {
-      if ( ifr.src.indexOf('dev2-bg.plan-vision.com') === -1 ) {
-        console.log("not connected. need to load");
-        ifr.src = "http://dev2-bg.plan-vision.com:3000";
-      } else {
-        console.log("main thread is good");
+      if ( res.status === 0 && ifr.src.indexOf('dev2-bg.plan-vision.com') > -1 ) {
+        console.log("connection to server seems fine");
       }
+
+      if ( res.status === 0 && ifr.src.indexOf('dev2-bg.plan-vision.com') === -1 ) {
+        console.log("not connected. need to load eet.");
+        ifr.src = "http://dev2-bg.plan-vision.com:3000";
+      } 
+
     }).catch(err => {
-      console.log("connection unavailable");
-      ifr.src ="loading.html";
-    }), 5* 1000);
+      console.log("interface down or connection totally unavailable.");
+      ifr.src ="loader.html";
+    }), 5 * 1000);
 }

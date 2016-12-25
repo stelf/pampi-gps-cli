c6bdb2acf9cedf000b34d1262c13d1c834355ff6
Laro DEE <larodi@gmail.com>
2016-12-25 19:05:26 +0200

    more updates to cron

--- oth/screenshot.sh
+++ oth/screenshot.sh
@@ -5,7 +5,20 @@ echo == setup recurring screenshot ==
 sudo apt install imagemagick
 sudo sh -c 'echo "*/5 * * * * DISPLAY=:0 import -window root /home/pi/current-screen.jpg -q 5" >> /var/spool/cron/crontabs/pi'
 
-sudo sh -c 'echo LC_ALL="en_GB.utf8" >> /etc/default/locale'
+if ! grep -q LC_ALL /etc/default/locale;
+then
+    echo "= Amend locale"
+    sudo sh -c 'echo LC_ALL="en_GB.utf8" >> /etc/default/locale'
+fi
+
+if ! sudo grep -q 1230 /var/spool/cron/crontabs/root;
+then
+    echo "= Amend cron"
+    sudo sh -c 'echo "0 1 * * * /sbin/reboot" >> /var/spool/cron/crontabs/root'
+    sudo sh -c 'echo "0 6 * * * /sbin/reboot" >> /var/spool/cron/crontabs/root'
+    sudo sh -c 'echo "0 1230 * * * /sbin/reboot" >> /var/spool/cron/crontabs/root'
+    sudo sh -c 'echo "0 18 * * * /sbin/reboot" >> /var/spool/cron/crontabs/root'
+fi
 
 echo == update wsh ==
 
@@ -15,4 +28,4 @@ git remote add bot ssh://visionr@dev2-bg.plan-vision.com:2299/wsh/
 for i in $(pgrep -f monitor); do kill $i; echo stopped $i; done 
 git pull bot HEAD
 rm /home/pi/wsh/js/external*.js
-
+    

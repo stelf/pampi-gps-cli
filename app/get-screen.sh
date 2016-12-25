dae789ebb626a588e42d15efc80a6f7bc7079822
Laro DEE <larodi@gmail.com>
2016-12-25 20:20:46 +0200

    asdf

--- app/get-screen.sh
+++ app/get-screen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 DISPLAY=:0 import -window root /home/pi/current-screen.jpg
-scp -P2299 current-screen.jpg pi-chan@dev2-bg.plan-vision.com:~/screens/`cat /etc/hostname`.jpg
+scp -P2299 /home/pi/current-screen.jpg pi-chan@dev2-bg.plan-vision.com:~/screens/`cat /etc/hostname`.jpg
